e576331ff5a7ed423ee994599dabfde104ab9d71
BaccanoB <beatrice.baccano@yahoo.it>
2021-04-27 15:05:37 +0200

    divisione passaggi

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// chiedere all'utente il numero di km
+
+// chiedere età del passeggero
+
+// calcolo prezzo del biglietto (0,21 euro a km)
+
+// applicazione sconti al biglietto
+
+// arrotondare prezzo con massimo due decimali
+
+// visualizzare prezzo in pagina
+
+
+
+
